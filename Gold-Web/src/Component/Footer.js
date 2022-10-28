0869b3e3b4ccd7b128c2834d5400d005e9075bb0
amaar raza <razaammar582@gmail.com>
2022-10-28 17:48:39 +0500

    updated

--- Gold-Web/src/Component/Footer.js
+++ Gold-Web/src/Component/Footer.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from './Images/logo.png'
+import  {AiFillGithub} from 'react-icons/ai'
+import   {AiFillTwitterCircle}from 'react-icons/ai'
+import  {BsFacebook}from 'react-icons/bs'
+import  {AiFillLinkedin}from 'react-icons/ai'
+
+
 const Footer = () => {
   return (
       <div className="footer">
@@ -14,16 +20,30 @@ const Footer = () => {
                        <div className="footer_box">
                               <h2>Useful Link</h2>
                                   <small>Home</small>
-                                  <small>Home</small>
-                                  <small>Home</small>
-                                  <small>Home</small>
-                                  <small>Home</small>
+                                  <small>About</small>
+                                  <small>Shop</small>
+                                  <small>Product</small>
+                                  <small>contact</small>
                        </div>
                        <div className="footer_box">
-                                        
+                       <h2>Quick Link</h2>
+                                  <small>Home</small>
+                                  <small>About</small>
+                                  <small>Shop</small>
+                                  <small>Product</small>
+                                  <small>contact</small>
                        </div>
                       <div className="footer_box">
-                                        
+                            <h2>Newsletter</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur 
+                              adipisicing elit. Quas, aperiam?</p>
+                              <input type="email" placeholder='Enter email' /> 
+                              <div className="icons">
+                                 <AiFillGithub/>
+                                 <AiFillTwitterCircle/>
+                                 <BsFacebook/>
+                                 <AiFillLinkedin/>
+                                 </div>           
                        </div>
                        <div className="footer_box">
                                         
